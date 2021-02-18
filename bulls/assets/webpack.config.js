c9f2d71bda7b68a3634b83de6ed673a8cccbff0f
kriti-g <kriti7000@gmail.com>
2021-02-18 00:16:14 -0500

    Update webpack.config.js

--- bulls/assets/webpack.config.js
+++ bulls/assets/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = (env, options) => {
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+      new MiniCssExtractPlugin({ filename: './css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
     .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
