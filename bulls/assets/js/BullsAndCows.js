a438f3b1dafd33952c5f3a809c12f14baf5ea524
kriti-g <kriti7000@gmail.com>
2021-02-17 23:07:56 -0500

    Update BullsAndCows.js

--- bulls/assets/js/BullsAndCows.js
+++ bulls/assets/js/BullsAndCows.js
@@ -15,7 +15,7 @@ function GameOver(props) {
     <p>The number was {props.number}.</p>
     <p>Play again?</p>
     <button class="btn btn-success" onClick={props.onClick} type="button">
-      Reset
+    Reset
     </button>
     </div>
     </div>
@@ -26,15 +26,16 @@ function Victory(props) {
   return (
     <div class="row">
     <div class="col-sm-6">
-        <h1>You won!</h1>
-        <br/>
-        <p>The number was {props.number}.</p>
-        <p>Play again?</p>
-        <button class="btn btn-success" onClick={props.onClick} type="button">
-          Reset
-        </button>
+    <h1>You won!</h1>
+    <br/>
+    <p>The number was {props.number}.</p>
+    <p>Play again?</p>
+    <button class="btn btn-success" onClick={props.onClick} type="button">
+    Reset
+    </button>
     </div>
-    </div>);
+    </div>
+  );
 }
 
 function GuessTable(props) {
@@ -43,12 +44,12 @@ function GuessTable(props) {
   }else {
     return (
       <tbody>
-        {props.state.guesses.map(guess => (
-          <tr id={guess.key}>
-            <td>{guess.value}</td>
-            <td>{guess.bulls}B{guess.cows}C</td>
-          </tr>
-        ))}
+      {props.state.guesses.map(guess => (
+        <tr id={guess.key}>
+        <td>{guess.value}</td>
+        <td>{guess.bulls}B{guess.cows}C</td>
+        </tr>
+      ))}
       </tbody>
     );
   }
@@ -77,17 +78,17 @@ function Controls({guess, reset}) {
   return (
     <div class="input-group mb-3">
     <input type="text" class="form-control"
-      value={text}
-      onChange={updateText}
-      onKeyPress={keyPress}
-      placeholder="Type a four-digit number here"/>
+    value={text}
+    onChange={updateText}
+    onKeyPress={keyPress}
+    placeholder="Type a four-digit number here"/>
     <div class="input-group-append">
-      <button class="btn btn-outline-danger" onClick={reset} type="button">
-        Reset
-      </button>
-      <button class="btn btn-success" onClick={onClick} type="button">
-        Guess
-      </button>
+    <button class="btn btn-outline-danger" onClick={reset} type="button">
+    Reset
+    </button>
+    <button class="btn btn-success" onClick={onClick} type="button">
+    Guess
+    </button>
     </div>
     </div>
   );
@@ -120,51 +121,54 @@ function BullsAndCows() {
   }
 
   let body = (
-  <div class="row">
-  <div class="col-sm-8">
-  <h1 class="display-4">Bulls and Cows</h1>
-  <Controls reset={resetGame} guess={guess}/>
-  <table class="table table-striped">
+    <div class="row">
+    <div class="col-sm-8">
+    <h1 class="display-4">Bulls and Cows</h1>
+    <Controls reset={resetGame} guess={guess}/>
+    <table class="table table-striped">
     <thead class="thead thead-light">
-      <tr>
-        <th>Guess</th>
-        <th>Result</th>
-      </tr>
+    <tr>
+    <th>Guess</th>
+    <th>Result</th>
+    </tr>
     </thead>
-      <GuessTable guesses={state}/>
-  </table>
-  </div>
-  <div class="col-sm-4">
-  <h1 class="display-4"> </h1>
-  <ol>
+    <GuessTable guesses={state}/>
+    </table>
+    </div>
+    <div class="col-sm-4">
+    <h1 class="display-4"> </h1>
+    <ol>
     <li>When you guess, the game will tell you how many bulls (B) and cows (C) you
     got in that guess.</li>
     <li>A bull means the right number in the right place, and a
     cow means the right number in the wrong place.</li>
     <li>A guess should not have duplicate digits or a starting 0.</li>
     <li>You get a maximum of 8 guesses.</li>
-  </ol>
-  <p>
-     Good luck!
-  </p>
-  </div>
-  </div>);
-
-  if (state.secret_revealed != "") {
-    if (state.guesses[state.guesses.length - 1].bulls == 4) {
-      body = (
-        <Victory number={state.secret_revealed} onClick={resetGame}/>);
-    } else if (state.guesses.length > 7) {
-      body = (
-      <GameOver number={state.secret_revealed} onClick={resetGame}/>);
-    }
-  }
-
-  return (
-    <div class="container p-3 my-3">
-    {body}
+    </ol>
+    <p>
+    Good luck!
+    </p>
     </div>
-  );
-}
+    </div>);
 
-export default BullsAndCows;
+    if (state){
+      if (state.secret_revealed != "") {
+        if (state.guesses[state.guesses.length - 1].bulls == 4) {
+          body = (
+            <Victory number={state.secret_revealed} onClick={resetGame}/>);
+          } else if (state.guesses.length > 7) {
+            body = (
+              <GameOver number={state.secret_revealed} onClick={resetGame}/>
+            );
+          }
+        }
+      }
+
+      return (
+        <div class="container p-3 my-3">
+        {body}
+        </div>
+      );
+    }
+
+    export default BullsAndCows;
